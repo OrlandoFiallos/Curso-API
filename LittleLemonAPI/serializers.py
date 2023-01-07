00ce7fa924a9df6501854b4052400d811270de6a
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-01-07 16:27:23 -0600

    Validación método 2:Usando argumentos de palabras clave en la clase Meta

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -8,16 +8,20 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','slug','title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    stock = serializers.IntegerField(source='inventory')
+    #stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     category = CategorySerializer(read_only = True)
     category_id = serializers.IntegerField(write_only=True) 
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
+    #price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
     
     class Meta:
         model = MenuItem
         #fields = ['id','title','price','inventory']
         fields = ['id','title','price','stock','price_after_tax','category','category_id']
+        extra_kwargs = {
+            'price':{'min_value':2},
+            'stock':{'source':'inventory', 'min_value':0}
+        }
         
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
