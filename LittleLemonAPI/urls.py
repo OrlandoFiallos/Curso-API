024bc69ce9cecac427dd046dff8122c0096ca817
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-01-06 20:33:34 -0600

    Antes de semana 3

---
+++ LittleLemonAPI/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('menu-items',views.menu_items),
+    # path('menu-items',views.MenuItemsView.as_view()),
+    # path('menu-items/<int:pk>',views.SingleMenuItemView.as_view()),
+    # path('categories/',views.CategoryView.as_view()),
+    # path('categories/<int:pk>',views.SingleCategoryItemView.as_view())
+]
