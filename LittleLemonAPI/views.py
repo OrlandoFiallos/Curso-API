429174f55e783637099deee8f53469160d61285b
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-01-09 15:00:22 -0600

    Vista basada en clase busqueda por un campo

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -72,3 +72,4 @@ class MenuItemsViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     ordering_fields = ['price','inventory']
+    search_fields = ['title']
